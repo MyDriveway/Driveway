b16c22e8c377251345dda1b27bc8f8656ef399e8
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-08-23 18:55:23 -0700

    webpack config setup

---
+++ build/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
