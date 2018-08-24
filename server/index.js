f7dc6f2c133780a125947a08ea12a7b51be8fb65
gerretkubota <gtkubota@cpp.edu>
2018-08-24 10:03:41 -0700

    Adding a connection to the database through server/index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
+require('dotenv/config');
+
 const express = require('express');
 const path = require('path');
-
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL, (err, db) => {
+  if(err) console.error(err);
+
+  console.log("connected to database");
+})
+
 app.use(express.static(path.join(__dirname +'./../'))); //serves the index.html
 
 app.listen(3000, () => {
