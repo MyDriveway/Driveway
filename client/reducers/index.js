3a215a1832fa11d975f7bcbf5255cda799862c54
Daniel Matuszak <danielmatuszak@Daniels-MacBook-Pro.local>
2018-08-24 15:36:12 -0700

    got the results to render according to test state

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 //import all reducers here
+import searchesReducer from './searchesReducer';
 
 //combine reducers
 const reducers = combineReducers({
-  //
+  searches: searchesReducer,
 });
 
 export default reducers;
