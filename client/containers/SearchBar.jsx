6df350fc1e53ab4eb9ef89d510631e044a9d4ff5
gerretkubota <gtkubota@cpp.edu>
2018-08-25 15:24:47 -0700

    able to generate the coordinates and the results

--- client/containers/SearchBar.jsx
+++ client/containers/SearchBar.jsx
@@ -72,6 +72,7 @@ class SearchBar extends Component {
     fetch(`/searchAddress/${input}`)
     .then(response => response.json())
     .then(data => {
+      console.log('data hereeee', data);
       this.props.addLocations(data.results)
       this.props.currLocation(data.coords)
     })
