b7c896f02f4369fd1b0f024d2fc0e6c2ff0dd091
seano <sth.xxx@gmail.com>
2018-08-28 10:44:57 -0700

    cleaned up

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,64 +1,59 @@
-import * as types from '../constants/actionTypes';
+import * as types from "../constants/actionTypes";
 
 //DRIVEWAY ACTIONS
 
-export const openCreateModal = () =>({
-  type: types.OPEN_CREATE_MODAL,
-})
-
-
-export const closeCreateModal = () =>({
-  type: types.CLOSE_CREATE_MODAL,
-})
-
-export const createDriveway = (data) =>({
+export const openCreateModal = () => ({
+  type: types.OPEN_CREATE_MODAL
+});
+export const closeCreateModal = () => ({
+  type: types.CLOSE_CREATE_MODAL
+});
+export const createDriveway = data => ({
   type: types.CREATE_DRIVEWAY,
-  payload: data,
-})
-
-export const creationError = () =>({
-  type: types.CREATION_ERROR,
-})
-
+  payload: data
+});
+export const creationError = () => ({
+  type: types.CREATION_ERROR
+});
 
 // add a location search value to the store
-export const addSearch = (userInput) => ({
+export const addSearch = userInput => ({
   type: types.ADD_SEARCH,
-  payload: userInput,
+  payload: userInput
 });
 
 // add a driveway to search locations
-export const addLocations = (locations) => ({
+export const addLocations = locations => ({
   type: types.ADD_LOCATIONS,
-  payload: locations,
+  payload: locations
 });
 
 // sets the current location fpr the map
-export const setCurrLocation = (currLocation) => ({
+export const setCurrLocation = currLocation => ({
   type: types.SET_CURR_LOCATION,
   payload: currLocation
-})
+});
 
 // select a specific marker on the map (currently broken in the Google maps container)
-export const selectMarker = (id) => ({
+export const selectMarker = id => ({
   type: types.SELECT_MARKER,
   payload: id
-})
+});
 
 // deselect any marker on the map
 export const deselect = () => ({
   type: types.DESELECT,
   payload: null
-})
+});
 
 // login being true bypases login page
-export const setLogin = (bool) => ({
+export const setLogin = bool => ({
   type: types.SET_LOGIN,
   payload: bool
-})
+});
 
 // sets all the markers to show on the map
-export const setMarkers = (markers) => ({
+export const setMarkers = markers => ({
   type: types.SET_MARKERS,
   payload: markers
-})
+});
