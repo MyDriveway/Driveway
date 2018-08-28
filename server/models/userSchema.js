7916485d817f1659082dae37560abe4fe7fa6003
ark234 <ark234@gmail.com>
2018-08-28 10:44:35 -0700

    adding unique constraint to username

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -2,34 +2,38 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  username: {type: String, required: true},
-  password: {type:String, require: true}
-})
+  username: { type: String, required: true, unique: true },
+  password: { type: String, require: true }
+});
 
 const SALT_WORK_FACTOR = 10;
 
 // automatically encrypts password
 userSchema.pre('save', function(next) {
   const that = this;
-  bcrypt.hash(that.password, SALT_WORK_FACTOR)
-  .then(function(hash) {
-    that.password = hash;
-    next();
-  }).catch(function(err) {
-    next(err);
-  })
-})
+  bcrypt
+    .hash(that.password, SALT_WORK_FACTOR)
+    .then(function(hash) {
+      that.password = hash;
+      next();
+    })
+    .catch(function(err) {
+      next(err);
+    });
+});
 
-// check user input password with actual password 
+// check user input password with actual password
 userSchema.methods.checkPassword = function(password) {
   const that = this;
   return new Promise(function(resolve, reject) {
-    bcrypt.compare(password, that.password)
-    .then(function(result) {
-      resolve(result);
-    }).catch(function(err) {
-      reject(err);
-    });
+    bcrypt
+      .compare(password, that.password)
+      .then(function(result) {
+        resolve(result);
+      })
+      .catch(function(err) {
+        reject(err);
+      });
   });
 };
 
