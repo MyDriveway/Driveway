a83e9de35143438d0a0ebac165d3e44b606dbb0a
seano <sth.xxx@gmail.com>
2018-08-28 20:53:00 -0700

    removed commented out code in Login

--- client/containers/Login.jsx
+++ client/containers/Login.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import * as actions from '../actions/actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import * as actions from "../actions/actions";
 
 const mapStateToProps = store => ({
   loggedIn: store.login.loggedIn
   // if the file requires a reducer which alters something in state, does that state element need to be passed as a state prop to the file?
-})
+});
 
 const mapDispatchToProps = dispatch => ({
-  setLogin: (bool) => dispatch(actions.setLogin(bool)),
-  setSignedUp: (bool) => dispatch(actions.setSignedUp(bool))
-})
+  setLogin: bool => dispatch(actions.setLogin(bool)),
+  setSignedUp: bool => dispatch(actions.setSignedUp(bool))
+});
 
 class Login extends Component {
   constructor(props) {
@@ -24,80 +24,75 @@ class Login extends Component {
     e.preventDefault();
 
     //getting input values and creating an object with them
-    const username = document.getElementById('username-login');
-    const password = document.getElementById('password-login');
-    
-    if (username && username.value !== '' && password && password.value !== '') {
+    const username = document.getElementById("username-login");
+    const password = document.getElementById("password-login");
+
+    if (
+      username &&
+      username.value !== "" &&
+      password &&
+      password.value !== ""
+    ) {
       const newLogin = {
         username: username.value,
         password: password.value
-      }
+      };
 
       //post request with user login info
-      fetch('/login', {
-        headers: {'Content-Type': 'application/json'},
-        method: 'POST',
+      fetch("/login", {
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
         body: JSON.stringify(newLogin)
-      }).then(response => {
-        if (response.status === 200) this.props.setLogin(true); // will cause page rerender
-        else {
-          username.value = '';
-          password.value = '';
-        }
-      }).catch(err => {
-        console.log(err);
       })
+        .then(response => {
+          if (response.status === 200) this.props.setLogin(true);
+          // will cause page rerender
+          else {
+            username.value = "";
+            password.value = "";
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 
   signup(e) {
     e.preventDefault();
     this.props.setSignedUp(false);
-
-    //getting input values and creating an object with them
-    // const username = document.getElementById('username-signup');
-    // const password = document.getElementById('password-signup');
-
-    // if (username && username.value !== '' && password && password.value !== '') {
-    //   const newSignup = {
-    //     username: username.value,
-    //     password: password.value
-    //   }
-
-    //   //post request with user signup info
-    //   fetch('/signup', {
-    //     headers: {'Content-Type': 'application/json'},
-    //     method: 'POST',
-    //     body: JSON.stringify(newSignup)
-    //   }).then(response => {
-    //     this.props.setLogin(true);
-    //   }).catch(err => {
-    //     console.log(err);
-    //   })
-    // }
   }
 
   render() {
     const style = {
       container: {
-        width: '100vw',
-        height: '100vh',
-        background: 'linear-gradient(to bottom right, rgba(35, 106, 98, 0.8),rgba(171, 105, 56, 0.8)'
+        width: "100vw",
+        height: "100vh",
+        background:
+          "linear-gradient(to bottom right, rgba(35, 106, 98, 0.8),rgba(171, 105, 56, 0.8)"
       }
-    }
+    };
     return (
-      <div style={style.container} className='flexRow'>
-        <div style={{width: '50%'}}>
-        <div style={{'display': 'inline-block', 'margin': '0px 10px'}}>
-          <h1 style={{"color": "#f4f4f4"}}>Login</h1><br />
-          <input id='username-login' type='text' placeholder='Username' /><br />
-          <input id='password-login' type='text' placeholder='Password' /><br />
-          <button onClick={this.login}>Login</button><br />
-          <button onClick={this.signup}>Make an account!</button><br />
-        </div>
+      <div style={style.container} className="flexRow">
+        <div style={{ width: "50%" }}>
+          <div style={{ display: "inline-block", margin: "0px 10px" }}>
+            <h1 style={{ color: "#f4f4f4" }}>Login</h1>
+            <br />
+            <input id="username-login" type="text" placeholder="Username" />
+            <br />
+            <input id="password-login" type="text" placeholder="Password" />
+            <br />
+            <button onClick={this.login}>Login</button>
+            <br />
+            <button onClick={this.signup}>Make an account!</button>
+            <br />
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
