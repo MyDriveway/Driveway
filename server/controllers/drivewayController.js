9f17677a09ed15ffbb24da2b743a32efffc82c79
ark234 <ark234@gmail.com>
2018-08-28 22:25:30 -0700

    added logic for generating schedule array based on timeStart and timeEnd. added userId to driveway object.

--- server/controllers/drivewayController.js
+++ server/controllers/drivewayController.js
@@ -1,60 +1,68 @@
-const Driveways = require('../models/driveways')
+const Driveways = require('../models/driveways');
 var googleMapsClient = require('@google/maps').createClient({
-    key: process.env.GOOGLE_API
-  });
+  key: process.env.GOOGLE_API
+});
 
 module.exports = {
-
-    create(req, res) {
-        const { address, city, state } = req.body;
-        if (req.file !== undefined) {
-            req.body.image = req.file.path
-        }
-        Driveways.findOne({ address: req.body.address }, (err, dbCheck) => {
-            if (err) return res.status(500).json({ error: '1 Internal Server Error'});
-
-            if (!dbCheck) {
-                req.body.city = city.toLowerCase();
-                req.body.state = state.toLowerCase();
-                googleMapsClient.geocode({ address: `${address}, ${city}, ${state}`}, function(err, response) {
-                    if (err) return res.status(500).json({ error: '2 Internal Server Error'});
-                    const locationObject = response.json.results[0].geometry.location
-                    req.body.geometry = {coordinates: [locationObject.lng, locationObject.lat]}
-                    const newDriveway = new Driveways(req.body)
-                    newDriveway.save(function (err) {
-                        if (err) return res.status(500).json({ error: err});
-                        return res.status(200).json(newDriveway)
-                    })
-                  });
-            } else {
-                return res.status(400).json({
-                    error: 'Address has an active posting.'
-                  })
-            }
-        })
-    },
-
-    search(req, res) {
-        Driveways.aggregate(
-            [
-              {
-                $geoNear: {
-                  near: {
-                    type: 'Point',
-                    coordinates: [parseFloat(req.params.lng), parseFloat(req.params.lat)]
-                  },
-                  distanceField: 'dist',
-                  includeLocs: 'dist.location',
-                  maxDistance: parseFloat(8050),
-                  spherical: true,
-                }
-              }
-            ], (err, result) => {
-                console.log('result here', result);
-              if(err) return res.status(500).send(err);
-      
-              res.send(JSON.stringify(result));
-            }
-        )
+  create(req, res) {
+    // console.log('SSID ====>', req.cookies.ssid);
+    console.log('req.body =====>', req.body);
+    const { address, city, state } = req.body;
+    if (req.file !== undefined) {
+      req.body.image = req.file.path;
     }
-}
+    Driveways.findOne({ address: req.body.address }, (err, dbCheck) => {
+      if (err) return res.status(500).json({ error: '1 Internal Server Error' });
+
+      if (!dbCheck) {
+        req.body.city = city.toLowerCase();
+        req.body.state = state.toLowerCase();
+        googleMapsClient.geocode({ address: `${address}, ${city}, ${state}` }, function(err, response) {
+          if (err) return res.status(500).json({ error: '2 Internal Server Error' });
+          const locationObject = response.json.results[0].geometry.location;
+          req.body.geometry = { coordinates: [locationObject.lng, locationObject.lat] };
+          req.body.userId = req.cookies.ssid;
+          req.body.schedule = [];
+          for (let i = 0; i < 24; i++) {
+            if (i >= req.body.timeStart * 1 && i <= req.body.timeEnd) req.body.schedule.push(1);
+            else req.body.schedule.push(0);
+          }
+          const newDriveway = new Driveways(req.body);
+          newDriveway.save(function(err) {
+            if (err) return res.status(500).json({ error: err });
+            return res.status(200).json(newDriveway);
+          });
+        });
+      } else {
+        return res.status(400).json({
+          error: 'Address has an active posting.'
+        });
+      }
+    });
+  },
+
+  search(req, res) {
+    Driveways.aggregate(
+      [
+        {
+          $geoNear: {
+            near: {
+              type: 'Point',
+              coordinates: [parseFloat(req.params.lng), parseFloat(req.params.lat)]
+            },
+            distanceField: 'dist',
+            includeLocs: 'dist.location',
+            maxDistance: parseFloat(8050),
+            spherical: true
+          }
+        }
+      ],
+      (err, result) => {
+        console.log('result here', result);
+        if (err) return res.status(500).send(err);
+
+        res.send(JSON.stringify(result));
+      }
+    );
+  }
+};
